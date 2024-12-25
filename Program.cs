3b1fa98fadf168ae392db2b4e06350b1bd0c5a0a
Mustafa Dogan <mdogan89@hotmail.com>
2024-12-26 00:30:45 +0300

    Proje dosyası ekle.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+try
+{
+
+    Console.WriteLine("Bir sayı giriniz: ");
+    int sayi = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("sayi: " + sayi);
+
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message.ToString());
+	
+}
+finally
+{
+    Console.WriteLine("işlem tamamlandı");
+}
+
+
+try
+{
+    int a = int.Parse("10000000000");
+}
+catch (ArgumentNullException ex)
+{
+    Console.WriteLine("bos deger girdiniz");
+    Console.WriteLine(ex);
+}
+catch (FormatException ex)
+{
+    Console.WriteLine(ex.Message.ToString());
+}
+catch (OverflowException ex)
+
+
+{
+
+    Console.WriteLine("Cok kücük ya da büyük bir deger girdiniz");
+    Console.WriteLine(ex.Message.ToString());
+}
+finally { Console.WriteLine("islem tamamlandı"); }
+
